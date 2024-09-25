94bf78f6a839f933764e4be73ca825aed55ee0f8
Asfand322 <asfandsheikh321@gmail.com>
2024-09-25 13:58:44 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# push-sort-array
+ 
---
+++ index.js
@@ -0,0 +1,35 @@
+// Push method
+
+var a = []
+a.push("asfand")
+document.write(a)
+
+// it will add asfand in a 
+
+var b = [1,2]
+b.push("asfand")
+document.write(b)
+
+// it will show 1,2,asfand
+// which means it will add at last
+
+// Sort method
+// means in sequence order
+
+
+var c = [44,55,22]
+c.sort()
+document.write(c)
+
+// it will show 22,44,55
+// which means it wil sort in sequence order
+
+// REVERSE METHOD
+// START FROM Z TO A
+
+var d = ["h","x","a","d"]
+d.reverse()
+document.write(d)
+
+// it will show d,a,x,h
+// means in reverse order
